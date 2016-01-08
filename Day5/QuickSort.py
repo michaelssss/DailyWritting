c5361aaca9c2bcef869b0797fb1ea04e54bf280d
michaelssss <liangyuming2@gmail.com>
2016-01-08 11:44:24 +0800

    Day-5 writing

--- Day5/QuickSort.py
+++ Day5/QuickSort.py
@@ -2,4 +2,20 @@ class QuickSort(object):
     def __init__(self):
         self.list = [2, 1, 3, 5, 4, 6]
 
-    def dive(self):
+    def quicksort1(self, arr):
+
+        if len(arr) <= 1:
+            return arr
+        arr1 = []
+        arr2 = []
+        for index in arr:
+            if index > arr[0]:
+                arr1.append(index)
+            if index < arr[0]:
+                arr2.append(index)
+        return self.quicksort1(arr2) + [arr[0]] + self.quicksort1(arr1)
+
+
+quicksort = QuickSort()
+
+print(quicksort.quicksort1(quicksort.list))
